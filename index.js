14c8e9723f010af4599af9dbaae4e8713e7949cf
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-07-28 21:50:29 -0500

    Made conection with DB in render

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express');
+import pool from './src/db.js';
+import express from 'express';
 const app = express();
 const port = 3000;
 
-
+async function obtenerRutas() {
+    try {
+        const res = await pool.query ('SELECT * FROM ruta');
+        console.log('Resultados de la consulta', res.rows);
+        return res.rows;
+    }
+    catch{
+        console.error('Error ejecutando la consulta', err.stack);
+        throw err
+    }
+}
 app.get('/', (req, res) => {
     res.send('App de express iniciada')
 });
 
 app.listen(port, () =>{
     console.log('Servidor escuchando en puerto 3000')
+    
 })
+
+obtenerRutas()
