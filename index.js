b420fea597ad49921f682c5d5f074048a78e0b5f
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-08-02 19:31:05 -0500

    Revert "Merge pull request #3 from SaidNa14-02/Feature/ModeloUsuario"
    This reverts commit 05ab520e89e5c99e45d9238dc7d82a15d06b4710, reversing
    changes made to 3a3537558629d606cf9b97bf0aa14864106f5091.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-import express from 'express';
-import rutaRoutes from './routes/rutaRoutes.js'
-import usuarioRoutes from './routes/usuarioRoutes.js'
+const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware para parsear JSON
-app.use(express.json());
 
-// Rutas
-app.use('/api/rutas', rutaRoutes);
-app.use('/api/usuarios', usuarioRoutes);
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en puerto ${port}`);
+app.get('/', (req, res) => {
+    res.send('App de express iniciada')
 });
+
+app.listen(port, () =>{
+    console.log('Servidor escuchando en puerto 3000')
+})
