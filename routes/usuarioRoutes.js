1980d563b7ce6413c049a3df72040a651df4ed38
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-08-11 21:15:07 -0500

    Added validations to User and passwords

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import {createUsuario, getAllUsuarios, deleteUsuario, updateUsuario, loginUsuario, changePassword, getMyProfile, getPublicProfile} from '../controllers/usuarioController.js'
+import {createUsuario, getAllUsuarios, deleteUsuario, updateUsuario, loginUsuario, changePassword, getMyProfile, getPublicProfile} from '../controllers/usuarioController.js';
 import { isauthenticated } from '../middleware/authMiddleware.js';
+import { usuarioValidate, updateProfileValidate, changePasswordValidate } from '../validators/usuarioValidation.js';
 
 const router = express.Router();
 
-router.post('/register', createUsuario);
+router.post('/register', usuarioValidate, createUsuario);
 router.get('/', isauthenticated, getAllUsuarios);
 router.delete('/:id', isauthenticated, deleteUsuario);
-router.patch('/:id', isauthenticated, updateUsuario);
-router.patch('/me/password', isauthenticated, changePassword);
+router.patch('/:id', isauthenticated, updateProfileValidate, updateUsuario);
+router.patch('/me/password', isauthenticated, changePasswordValidate, changePassword);
 router.get('/me/profile', isauthenticated, getMyProfile);
 router.post('/login', loginUsuario);
 router.get('userprofile/:id', isauthenticated, getPublicProfile)
