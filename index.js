e595840d73016fbfdfcce70a96c33b7d55a39a83
SaidNa14-02 <msnavarretem@puce.edu.ec>
2025-08-13 10:25:21 -0500

    Added helmet to add a security layer to request headers

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ import usuarioRoutes from './routes/usuarioRoutes.js';
 import viajeCompartidoRoutes from './routes/viajeRoutes.js';
 import reservaRoutes from './routes/reservaRoutes.js';
 import cors from 'cors';
+import helmet from 'helmet';
 const app = express();
 app.use(cors());
 const port = 3000;
 
 // Middleware para parsear JSON
 app.use(express.json());
-
+//Para el proyecto, las configuraciones por defecto de Helmet son suficientes
+app.use(helmet())
 // Rutas
 app.use('/api/rutas', rutaRoutes);
 app.use('/api/usuarios', usuarioRoutes);
